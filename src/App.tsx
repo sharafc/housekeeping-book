bbdca651be0ede92250082ad3d8700e9ff8cb37f
Christian Sharaf <sharafc@users.noreply.github.com>
2020-11-29 11:30:19 +0100

    Add TypeScript notation and Netlify config

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { ReactElement } from 'react';
 
-function App() {
+function App(): ReactElement {
   return (
     <div className="App">
       <header className="App-header">
