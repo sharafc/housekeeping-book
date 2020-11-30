42f3f94648af4d22fc71dd28211f0b0ea1eb2098
Christian Sharaf <sharafc@users.noreply.github.com>
2020-11-30 12:02:00 +0100

    Add debug logging  since env doesn't work on host

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -2,12 +2,14 @@ import React, { ReactElement } from "react";
 import logo from "../../statics/image/logo.svg";
 
 function Home(): ReactElement {
+
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>Working hard on Version 1 of the Household Account Book</p>
             </header>
+            {console.log(process.env.REACT_APP_PATH_TO_HASURA)}
         </div>
     );
 }
